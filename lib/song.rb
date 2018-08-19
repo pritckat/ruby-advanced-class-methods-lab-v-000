bf1f4435a139fbfb18f047c81c6eace61ff432db
Katrina Pritchard <pritckat@gmail.com>
2018-08-19 21:33:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,6 +8,50 @@ class Song
 
   def save
     self.class.all << self
+    self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(title)
+    song = self.new
+    song.name = title
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.new
+    @@all << song
+    song.name = song_name
+    song
+  end
+
+  def self.find_by_name(song_name)
+    @@all.each do |song|
+      return song if song.name == song_name
+    end
+    nil
+  end
+
+  def self.find_or_create_by_name(song_name)
+    if self.find_by_name(song_name) == nil
+      song = self.create_by_name(song_name)
+    else
+      self.find_by_name(song_name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|a| a.name}
+  end
+
+  def self.new_from_filename(filename)
+    song = self.new
+    filename.split(/[-.]/)
+    puts filename
+  end
 end
